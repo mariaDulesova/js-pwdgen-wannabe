32a139547f7d021ade9e7b76e6155a105dcbe609
mariaDulesova <maria.dulesova@gmail.com>
2021-04-26 14:23:47 +0200

    password generator

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+// Chiedi all'utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+
+
+var userName = prompt("Mi dici il tuo nome?");
+console.log(userName);
+
+var userLastName = prompt("Mi dici il tuo cognome?");
+console.log(userLastName);
+
+var userFavoriteColor = prompt("Mi dici il tuo colore preferito");
+console.log(userFavoriteColor);
+
+document.getElementById("password").innerHTML = userName + userLastName + userFavoriteColor + "21";
